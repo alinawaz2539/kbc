ca9d6c162aa034aee0aed97aa7a8b41c7d46c478
ALI NAWAZ HAIDER RIZVI <alinawazhaider351@gmail.com>
2020-11-07 14:34:56 +0530

    Please check

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 void main() {
   runApp(MaterialApp(home: Home()));
@@ -12,68 +15,156 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<String> question = [
-    'Q1. Who was the first person to walk on the moon ?',
-    'Q2. There are how many continents in the World ?',
-    'Q3. There are how many Union Territories in India ?',
-    'Q4. In RGB, What does the R stands for ?',
+  var questions = [
+    {
+      'question': 'How many states are there in India',
+      'answers': ['20', '25', '28', '29'],
+      'correctAnswer': '29'
+    },
+    {
+      'question': 'How many union territories are there in India',
+      'answers': ['7', '8', '28', '29'],
+      'correctAnswer': '8'
+    },
+    {
+      'question': 'How many days are there in a week',
+      'answers': ['7', '8', '28', '29'],
+      'correctAnswer': '7'
+    },
+    {
+      'question': 'How many alphabets are there in english',
+      'answers': ['7', '8', '26', '29'],
+      'correctAnswer': '26'
+    },
+    {
+      'question': 'How many states are there in India',
+      'answers': ['20', '25', '28', '29'],
+      'correctAnswer': '29'
+    },
+    {
+      'question': 'How many union territories are there in India',
+      'answers': ['7', '8', '28', '29'],
+      'correctAnswer': '8'
+    },
+    {
+      'question': 'How many days are there in a week',
+      'answers': ['7', '8', '28', '29'],
+      'correctAnswer': '7'
+    },
+    {
+      'question': 'How many alphabets are there in english',
+      'answers': ['7', '8', '26', '29'],
+      'correctAnswer': '26'
+    },
+    {
+      'question': 'How many states are there in India',
+      'answers': ['20', '25', '28', '29'],
+      'correctAnswer': '29'
+    },
+    {
+      'question': 'How many union territories are there in India',
+      'answers': ['7', '8', '28', '29'],
+      'correctAnswer': '8'
+    },
+    {
+      'question': 'How many days are there in a week',
+      'answers': ['7', '8', '28', '29'],
+      'correctAnswer': '7'
+    },
+    {
+      'question': 'How many alphabets are there in english',
+      'answers': ['7', '8', '26', '29'],
+      'correctAnswer': '26'
+    },
+    {
+      'question': 'How many states are there in India',
+      'answers': ['20', '25', '28', '29'],
+      'correctAnswer': '29'
+    },
+    {
+      'question': 'How many union territories are there in India',
+      'answers': ['7', '8', '28', '29'],
+      'correctAnswer': '8'
+    },
+    {
+      'question': 'How many days are there in a week',
+      'answers': ['7', '8', '28', '29'],
+      'correctAnswer': '7'
+    },
   ];
-  Map<int, List<String>> options = {
-    0: [
-      'A. Isaac Newton',
-      'B. Kalpana Chawla',
-      'C. Sunita William',
-      'D. Neil Armstrong'
-    ],
-    1: ['A. 6', 'B. 7', 'C. 8', 'D. 9'],
-    2: ['A. 6', 'B. 7', 'C. 8', 'D. 9'],
-    3: ['A. Ranbow', 'B. Red', 'C. Rat', 'D. Right'],
-  };
-  Map<int, int> ans = {
-    0 : 3,
-    1 : 1,
-    2 : 2,
-    3 : 1,
-  };
-  int counter = 0;
-  int i=0;
-  int score=0;
-  Widget questionTemplate(String str, int i) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+  var amount = [
+    '₹ 00',
+    '₹ 5,000',
+    '₹ 10,000',
+    '₹ 20,000',
+    '₹ 40,000',
+    '₹ 80,000',
+    '₹ 1,60,000',
+    '₹ 3,20,000',
+    '₹ 6,40,000',
+    '₹ 12,50,000',
+    '₹ 25 Lakhs',
+    '₹ 50 Lakhs',
+    '₹ 1 Crore',
+    '₹ 3 Crore',
+    '₹ 5 Crore',
+    '₹ 7 Crore'
+  ];
+  int _questionIndex = 0;
+  int i = 0;
+  int score = 0;
+  int total = 15;
+  bool flag = true;
+  final player = AudioCache();
+  Widget build(BuildContext context) {
+    if (_questionIndex == 15) {
+      return Card(
+          child: Scaffold(
+        backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('KBC'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FlatButton(onPressed: (){
-              setState(() {
-                if(counter <4 && ans[counter]==i){
-                  score++;
-                }
-                i=0;
-                counter += 1;
-              });
-            },
-            child: Text(str,
-                style: TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.purple[600],
-                )),
-            color: Colors.white,
-            hoverColor: Colors.black,
-            splashColor: Colors.green,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+                  child: Text('Congratulations!',
+                      style: TextStyle(
+                        fontSize: 44.0,
+                        color: Colors.black87,
+                      ))),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+                  child: Text('You Have Won 7 crore Rupees!',
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        color: Colors.green[400],
+                      ))),
             ),
           ],
         ),
-      ),
-    );
-  }
-
-  
-  @override
-  Widget build(BuildContext context) {
-    if (counter < 4) {
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            player.play('audio/clicksound.mp3');
+            setState(() {
+              score = 0;
+              i = 0;
+              _questionIndex = 0;
+            });
+          },
+          label: Text('Play Again'),
+        ),
+      ));
+    } else if (flag) {
       return Scaffold(
           backgroundColor: Colors.grey[300],
           appBar: AppBar(
@@ -81,58 +172,190 @@ class _HomeState extends State<Home> {
             title: Text('KBC'),
           ),
           body: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
-                child: Text(question[counter],
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      color: Colors.black,
-                    )),
-              ),
-      
-              for (i = 0; i < 4; i++)
-                questionTemplate(options[counter][i], i),
-            ],
-          ),
-          
-          );
-    } 
-    else {
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.bottomRight,
+                  margin: EdgeInsets.fromLTRB(16.0,32.0, 16.0, 2.0),
+                  child: Text(amount[_questionIndex+1],
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.green,
+                      )),
+                ),
+
+                Container(
+                  margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+                  child: Text(
+                      'Q${_questionIndex + 1}. ${questions[_questionIndex]['question']}',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.black,
+                      )),
+                ),
+
+                ...(questions[_questionIndex]['answers'] as List<String>)
+                    .map((option) {
+                  return GestureDetector(
+                    child: Card(
+                      margin: EdgeInsets.all(8),
+                      child: ListTile(
+                        title: Text(option),
+                      ),
+                    ),
+                    onTap: () {
+                      player.play('audio/clicksound.mp3');
+                      if (questions[_questionIndex]['correctAnswer'] !=
+                          option) {
+                        flag = false;
+                      } else {
+                        _questionIndex++;
+                        // RenderErrorBox.backgroundColor.green;
+                      }
+
+                      setState(() {});
+                    },
+                  );
+                }),
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  margin: EdgeInsets.fromLTRB(16.0,32.0, 16.0, 2.0),
+                  child: Text('Won:- ${amount[_questionIndex]}',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.green,
+                      )),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+                  width: double.infinity,
+                  height: 50,
+                  child: LinearPercentIndicator(
+                    width: 350,
+                    lineHeight: 14.0,
+                    percent: (100 / total) / 100 * _questionIndex,
+                    backgroundColor: Colors.grey,
+                    progressColor: Colors.green,
+                  ),
+                )
+                //  for (i = 0; i < 4; i++)
+                // questionTemplate(options[counter][i], i),
+              ]));
+    } else {
       return Card(
-       child: Scaffold(
+          child: Scaffold(
         backgroundColor: Colors.grey[300],
         appBar: AppBar(
           centerTitle: true,
           title: Text('KBC'),
         ),
-       
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+                  child: Text('Wrong Answer!',
+                      style: TextStyle(
+                        fontSize: 44.0,
+                        color: Colors.black87,
+                      ))),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+                  child: Text('You Won ${amount[_questionIndex]}',
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        color: Colors.red[400],
+                      ))),
+            ),
+            /* SafeArea(
+              child: FlatButton(
+                child: Text("play "),
+                onPressed: () {
+                  player.play('audio/clicksound.mp3');
+                },
+              ),
+            ),*/
+          ],
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            player.play('audio/clicksound.mp3');
+            setState(() {
+              score = 0;
+              i = 0;
+              flag = true;
+              _questionIndex = 0;
+            });
+          },
+          label: Text('Play Again'),
+        ),
+      ));
+    }
+  }
+}
+/*
+  @override
+  Widget build(BuildContext context) {
+    if (counter < 4) {
+      return Scaffold(
+        backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('KBC'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+              child: Text(questions[counter],
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.black,
+                  )),
+            ),
+            for (i = 0; i < 4; i++) questionTemplate(options[counter][i], i),
+          ],
+        ),
+      );
+    } else {
+      return Card(
+          child: Scaffold(
+        backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('KBC'),
+        ),
         body: Column(
           children: <Widget>[
             questionTemplate('Congrats!!!', 0),
             questionTemplate('Your score = $score / 4', 0),
-            
           ],
         ),
         floatingActionButton: FloatingActionButton.extended(
           onPressed: () {
             setState(() {
-              score=0;
-              i=0;
+              score = 0;
+              i = 0;
               counter = 0;
             });
           },
           label: Text('Play Again'),
         ),
-      )
-      );
-      
-
+      ));
     }
     // This trailing comma makes auto-formatting nicer for build methods.
   }
 }
-/*floatingActionButton: Padding(
+floatingActionButton: Padding(
             padding: const EdgeInsets.fromLTRB(32.0, 0.0, 0.0, 0.0),
             child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,230 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hello_flutter/main.dart';
+class LoginView extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  final _fnMobileNumber = FocusNode();
+  final _fnEmailAddress = FocusNode();
+  final _fnPassword = FocusNode();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  String _mobileNumber = '';
+  String _emailAddress = '';
+  String _password = '';
+  
+  processLoginCredentials() {
+    if (_formKey.currentState.validate()) {
+      //this ensures that onSaved callbacks get invoked
+      _formKey.currentState.save();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      //connect with login api with credentials as input
+      print('Mobile Number: $_mobileNumber');
+      print('Email Address: $_emailAddress');
+      print('Password: $_password');
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+      //received response is processed
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign-In'),
+      ),
+      body: Card(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    autofocus: true,
+                    decoration: InputDecoration(
+                        labelText: 'Mobile Number',
+                        prefixIcon: Icon(Icons.smartphone),
+                        prefixText: '+91-'),
+                    keyboardType: TextInputType.phone,
+                    maxLength: 10,
+                    maxLengthEnforced: true,
+                    focusNode: _fnMobileNumber,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter your mobile no.';
+                      }
+                      return null;
+                    },
+                    textInputAction: TextInputAction.next,
+                    onSaved: (strValue) {
+                      _mobileNumber = strValue;
+                    },
+                    onFieldSubmitted: (value) {
+                      FocusScope.of(context).requestFocus(_fnEmailAddress);
+                    },
+                  ),
+                  TextFormField(
+                    //autofocus: true,
+                    focusNode: _fnEmailAddress,
+                    keyboardType: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.next,
+                    decoration: InputDecoration(
+                      hintText: 'Email Address',
+                      prefixIcon: Icon(Icons.email),
+                    ),
+                    validator: (strValue) {
+                      if (strValue.isEmpty == true && strValue.length > 20)
+                      {
+                        return 'Please enter your email address';
+                      }
+                      else{
+                        bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(strValue);
+                        if(emailValid == false){
+                          return 'Please enter the correct email  address';
+                        }
+                      }
+                      return null;
+                    },
+                    onSaved: (strValue) {
+                      _emailAddress = strValue;
+                    },
+                    onFieldSubmitted: (strValue) {
+                      FocusScope.of(context).requestFocus(_fnPassword);
+                    },
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    focusNode: _fnPassword,
+                    keyboardType: TextInputType.visiblePassword,
+                    textInputAction: TextInputAction.done,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      prefixIcon: Icon(Icons.vpn_key),
+                    ),
+                    validator: (strValue) {
+                      if (strValue.isEmpty == true)
+                        return 'Please enter your password';import 'package:flutter/material.dart';
+
+class RegistrationSuccessful extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appTitle = 'Form Validation Demo';
+
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        //   appBar: AppBar(
+        //     title: Text(appTitle),
+        //   ),
+        body: MyCustomForm(),
+      ),
+    );
+  }
+}
+
+// Create a Form widget
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+class MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        alignment: Alignment.center,
+        color: Colors.grey[900],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              FloatingActionButton(onPressed: null,
+              backgroundColor: Colors.white,
+              child : Icon(
+                Icons.check,
+                size: 40,
+                color: Colors.black87,
+  
+              ),
+             ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 40,
+                  top: 30,
+                  right: 40,
+                  bottom: 20,
+                ),
+                child: Text(
+                  "Your account is \n registered successfully",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.white,
+                    // fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                child: ButtonTheme(
+                  minWidth: 270.0,
+                  height: 50,
+                  child: RaisedButton(
+                    textColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    color: Colors.pink,
+                    child: Text(
+                      'LOGIN',
+                    ),
+                    onPressed: () {
+                      }
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+  }
+}
+
+                      return null;
+                    },
+                    onSaved: (strValue) {
+                      _password = strValue;
+                    },
+                    onFieldSubmitted: (strValue) {
+                      processLoginCredentials();
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 16,
+                    ),
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: RaisedButton(
+                      onPressed: processLoginCredentials,
+                      child: Text('LOGIN'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
